a09fcf5d26d751f5b4707e13ead5af01eb286f57
motorolla29 <eutyou@gmail.com>
2023-06-18 23:38:40 +0300

    init spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,11 +4,14 @@ import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 
+import Spinner from '../spinner';
+
 export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
     planet: {},
+    loading: true,
   };
 
   constructor() {
@@ -17,44 +20,50 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({ planet });
+    this.setState({ planet, loading: false });
   };
 
   updatePlanet() {
-    const id = 12;
+    const id = Math.floor(Math.random() * 18 + 2);
     this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
   render() {
-    const {
-      planet: { id, name, population, rotationPeriod, diameter },
-    } = this.state;
-
+    const { planet, loading } = this.state;
     return (
       <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-image"
-          alt={`${id}-img`}
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {loading ? <Spinner /> : <PlanetView planet={planet} />}
       </div>
     );
   }
 }
+
+const PlanetView = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+  return (
+    <>
+      <img
+        className="planet-image"
+        alt={`${id}-img`}
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+      />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-pulse-rucnzqcdcs">
+      <div class="ldio-cy9opyhgci">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
